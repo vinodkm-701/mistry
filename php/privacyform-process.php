831062253880546f076b5a75e7babb7ec14468dd
vinodkm-701 <71050779+vinodkm-701@users.noreply.github.com>
2021-02-26 18:25:14 +0530

    Update privacyform-process.php
    email update

--- php/privacyform-process.php
+++ php/privacyform-process.php
@@ -25,7 +25,7 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
+$EmailTo = "vinod.vinod797@gmail.com";
 $Subject = "New privacy request from Evolo landing page";
 
 // prepare email body text
